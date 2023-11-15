aeeef4a68b44a48e455c11635018d66a6e348e42
gdegough <gdegough@gmail.com>
2023-11-14 21:27:17 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   flake.lock
    	modified:   home-manager/zsh-config.nix
    	modified:   nixos/users.nix

--- nixos/users.nix
+++ nixos/users.nix
@@ -20,8 +20,8 @@ in
 #  };
   users.users.root = {
     shell = pkgs.bash;
-    passwordFile = "/persist/passwords/root";
-#    passwordFile = config.sops.secrets.root-password.path;
+    hashedPasswordFile = "/persist/passwords/root";
+#    hashedPasswordFile = config.sops.secrets.root-password.path;
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCmhJIl7WJNvnigGjC6rSXxeaTQxy7OSRzSKbaWIUpEYgZE5BJ+vlVOgp8ZxP4D9ruZp2RSmbKJs6Ih68GaQiccof42VVYM2QF4MtieXISjn2+dvvwLcRerkXmN4tA/8JMHX56lMVwF2NvaTs41NZSeWcnlhuQ8iRQJiHCuj18ikiG2ob1Qo1iHu6pSI2twQlONmgbOB5ZmHONq1mCBWPvNUdo50oRdcx0h3EjiKXJyTgDfR5bhidfzVr/5e9fLCiHg+wIQuzJlRRbge9ywpyZhv1szUz1TlSjd4QMUj4MGCu2R1GCspYdfY56PMoQatQRdOVOxiV2fXOd4I7U8pB0rUdd4n3Z20hmmOYCirfFn3ZZwMjWF6gDHg50KuGuwUycIcpFCC6y2Vr24MzRlbtsqzzyaQA2czhtZzQMN68nPUmGNB1WYvHmt/k5R2rMvdRf0TOg8TciSkWCt/bFixOpVB5pxoKgHgqDHi/eMsksbsYEkAyStARVX5No0y3kjrqBurV7BM3BnhyfefofpHSIVFwIFlZD/LTJ1eQKNyIVDFDH/2DkuMrS4fEbFXuQ4T+mLzD1KLLU8Xg/whonKAJ7O6UliMHLBp+vqfm1hT24z4LPgy9b5oyFeSofMYDZfHn5DF9nCgX/QttyYQ281Xm0vF/dX6umcHKA7HkoFCzeFIQ== root"
     ];
@@ -48,8 +48,8 @@ in
       "shared-files" 
     ];
     shell = pkgs.bash;
-    passwordFile = "/persist/passwords/gmdegoug";
-#    passwordFile = config.sops.secrets.gmdegoug-password.path;
+    hashedPasswordFile = "/persist/passwords/gmdegoug";
+#    hashedPasswordFile = config.sops.secrets.gmdegoug-password.path;
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCmhJIl7WJNvnigGjC6rSXxeaTQxy7OSRzSKbaWIUpEYgZE5BJ+vlVOgp8ZxP4D9ruZp2RSmbKJs6Ih68GaQiccof42VVYM2QF4MtieXISjn2+dvvwLcRerkXmN4tA/8JMHX56lMVwF2NvaTs41NZSeWcnlhuQ8iRQJiHCuj18ikiG2ob1Qo1iHu6pSI2twQlONmgbOB5ZmHONq1mCBWPvNUdo50oRdcx0h3EjiKXJyTgDfR5bhidfzVr/5e9fLCiHg+wIQuzJlRRbge9ywpyZhv1szUz1TlSjd4QMUj4MGCu2R1GCspYdfY56PMoQatQRdOVOxiV2fXOd4I7U8pB0rUdd4n3Z20hmmOYCirfFn3ZZwMjWF6gDHg50KuGuwUycIcpFCC6y2Vr24MzRlbtsqzzyaQA2czhtZzQMN68nPUmGNB1WYvHmt/k5R2rMvdRf0TOg8TciSkWCt/bFixOpVB5pxoKgHgqDHi/eMsksbsYEkAyStARVX5No0y3kjrqBurV7BM3BnhyfefofpHSIVFwIFlZD/LTJ1eQKNyIVDFDH/2DkuMrS4fEbFXuQ4T+mLzD1KLLU8Xg/whonKAJ7O6UliMHLBp+vqfm1hT24z4LPgy9b5oyFeSofMYDZfHn5DF9nCgX/QttyYQ281Xm0vF/dX6umcHKA7HkoFCzeFIQ== gmdegoug"
     ];
@@ -81,7 +81,7 @@ in
       "pipewire" 
       "shared-files" 
     ];
-    passwordFile = "/persist/passwords/pdegough";
-#    passwordFile = config.sops.secrets.pdegough-password.path;
+    hashedPasswordFile = "/persist/passwords/pdegough";
+#    hashedPasswordFile = config.sops.secrets.pdegough-password.path;
   };
 }
