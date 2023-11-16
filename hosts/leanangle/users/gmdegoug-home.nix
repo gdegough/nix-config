4aace8b79a7fa8cc3a31c537854670482b0f6ff7
gdegough <gdegough@gmail.com>
2023-11-16 15:33:02 -0500

    On branch master  Your branch is ahead of 'origin/master' by 4 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   hosts/leanangle/users/gmdegoug-home.nix

--- hosts/leanangle/users/gmdegoug-home.nix
+++ hosts/leanangle/users/gmdegoug-home.nix
@@ -103,7 +103,7 @@
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git.enable = true;
-  programs.nvim.enable = true;
+  config.modules.editors.nvim.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
