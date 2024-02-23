01459180a155158e057f5db24f1854bd2fc99036
gdegough <gdegough@gmail.com>
2024-02-23 03:18:56 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/leanangle/hyprland-local-config.nix

--- home-manager/leanangle/hyprland-local-config.nix
+++ home-manager/leanangle/hyprland-local-config.nix
@@ -8,6 +8,13 @@
     ".config/hypr/monitor.conf".text = ''
       # See https://wiki.hyprland.org/Configuring/Monitors/
       monitor = HDMI-A-1, 3840x2160@59.997Hz, 0x0, 1.5
+
+      # unscale XWayland
+      xwayland { force_zero_scaling = true }
+
+      # toolkit-specific scale
+      env = GDK_SCALE,1.5
+      env = XCURSOR_SIZE,24
     '';
   };
 }
