513715f06ec12982d43b985b2a1e6cbf4b0e4366
gdegough <gdegough@gmail.com>
2024-02-26 07:28:53 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/music-composition.nix

--- home-manager/music-composition.nix
+++ home-manager/music-composition.nix
@@ -8,7 +8,7 @@
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
   home.packages = [
-    pkgs.unstable.frescobaldi
+    pkgs.frescobaldi
     pkgs.unstable.lilypond
   ];
 }
