609e265ca5c7f57aed3e2198412bb897707550b9
gdegough <gdegough@gmail.com>
2024-02-28 14:52:31 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   flake.nix
    	modified:   overlays/default.nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,17 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    #home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager-unstable.url = "github:nix-community/home-manager"; # Unstable
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    # home-manager-unstable.url = "github:nix-community/home-manager"; # Unstable
 
     # Add any other flake you might need
     # hardware.url = "github:nixos/nixos-hardware";
