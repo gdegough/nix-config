c97a6abaf2d6f73ab4c14dd0f933dcd2355c404f
gdegough <gdegough@gmail.com>
2024-02-29 08:39:32 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/music-composition.nix - import timidity
            config
    	new file:   home-manager/timidity-config.nix - added to include
            configuration of soundfont

--- home-manager/music-composition.nix
+++ home-manager/music-composition.nix
@@ -4,12 +4,14 @@
   ...
 }:
 {
+  imports = [
+    ./timidity-config.nix
+  ];
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
   home.packages = [
     pkgs.stable.frescobaldi
     pkgs.lilypond
-    pkgs.timidity
   ];
 }
---
+++ home-manager/timidity-config.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  programs.timidity = {
+    enable = true;
+    extraConfig = ''
+      soundfont ''${pkgs.soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2
+    '';
+  };
+}
