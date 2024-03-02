f0177694122a0e29063d8a2b6a0532498ce712d7
gdegough <gdegough@gmail.com>
2024-03-02 09:14:37 -0500

    On branch main  Your branch is ahead of 'origin/main' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   nixos/leanangle/configuration.nix

--- nixos/leanangle/configuration.nix
+++ nixos/leanangle/configuration.nix
@@ -119,7 +119,7 @@
 
   services.hardware.openrgb = {
     enable = true;
-    motherboard.type = [ "intel" ];
+    motherboard.type = "intel";
   };
   services.power-profiles-daemon.enable = true;
   services.printing.enable = true; # Enable CUPS to print documents.
