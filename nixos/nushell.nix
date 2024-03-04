9baffe7ac12ae5105d11ac4d05f0602832a1cf94
gdegough <gdegough@gmail.com>
2024-03-04 16:24:10 -0500

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   nixos/nushell.nix
    	modified:   nixos/zsh.nix

--- nixos/nushell.nix
+++ nixos/nushell.nix
@@ -11,10 +11,10 @@
     pkgs.nushell
     pkgs.carapace
   ];
-  programs = {
-    nushell.enable = true; # the nu shell
-    carapace.enable = true; # completion
-    carapace.enableNushellIntegration = true;
-  };
+  programs.nushell.enable = true; # the nu shell
+  programs.carapace.enable = true; # completion
+  programs.carapace.enableNushellIntegration = true;
+  programs.carapace.enableBashIntegration = true;
+  programs.carapace.enableZshIntegration = true;
   environment.pathsToLink = [ "/share/nushell" ];
 }
--- nixos/zsh.nix
+++ nixos/zsh.nix
@@ -7,8 +7,6 @@
   ...
 }: 
 {
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = [
     pkgs.zsh
     pkgs.zsh-autosuggestions
