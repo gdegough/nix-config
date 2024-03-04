29db01d495159333f87e4de1df9819418e84642a
gdegough <gdegough@gmail.com>
2024-03-04 18:58:10 -0500

    On branch main  Your branch is ahead of 'origin/main' by 10 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   home-manager/starship-config.nix

--- home-manager/starship-config.nix
+++ home-manager/starship-config.nix
@@ -11,7 +11,7 @@
     enable = true;
     settings = {
       add_newline = false;
-      format = "$line_break[($username@$hostname)](yellow bold)[\$ ]$character";
+      format = "$all";
       character = { 
         success_symbol = "[➜](bold green)";
         error_symbol = "[➜](bold red)";
