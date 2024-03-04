d3ce97ffa5b1817ca9c39748dd2d187a39646f17
gdegough <gdegough@gmail.com>
2024-03-04 16:35:18 -0500

    On branch main  Your branch is ahead of 'origin/main' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   home-manager/nushell-config.nix
    	modified:   nixos/nushell.nix

--- home-manager/nushell-config.nix
+++ home-manager/nushell-config.nix
@@ -7,6 +7,13 @@
   imports = [
     ./starship-config.nix
   ];
+  home.packages = [
+    pkgs.carapace
+  ];
+  programs.carapace = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
   programs.nushell = {
     # The config.nu can be anywhere you want if you like to edit your Nushell with Nu
     # configFile.source = ./.../config.nu;
--- nixos/nushell.nix
+++ nixos/nushell.nix
@@ -9,12 +9,7 @@
 {
   environment.systemPackages = [
     pkgs.nushell
-    pkgs.carapace
   ];
   programs.nushell.enable = true; # the nu shell
-  programs.carapace.enable = true; # completion
-  programs.carapace.enableNushellIntegration = true;
-  programs.carapace.enableBashIntegration = true;
-  programs.carapace.enableZshIntegration = true;
   environment.pathsToLink = [ "/share/nushell" ];
 }
