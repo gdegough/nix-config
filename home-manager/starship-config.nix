4fe03a5f4ee48b990fdd2d4c88d5a920b13cd60e
gdegough <gdegough@gmail.com>
2024-03-04 23:48:30 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/starship-config.nix

--- home-manager/starship-config.nix
+++ home-manager/starship-config.nix
@@ -11,9 +11,7 @@
     enable = true;
     settings = {
       add_newline = false;
-      format = "[┌](bold green)([─\\(](bold green)$os[\\)](bold green))" +
-        "([─\\(](bold green)$username)[@](bold yellow)($hostname[\\)](bold green))" +
-        "([─\\(](bold green)$directory[\\)](bold green))$line_break[└─](bold green)$character";
+      format = "┌(─\\($os\\))(─\\($username)->($hostname\\))(─\\($directory\\))$line_break└─$character";
       character = { 
         success_symbol = "[>](bold green)";
         error_symbol = "[>](bold red)";
@@ -24,13 +22,11 @@
       };
       hostname = {
         format = "[$ssh_symbol$hostname]($style)";
-        style = "bold yellow";
         trim_at = "";
         ssh_only = false;
       };
       os = {
         format = "[$symbol]($style)";
-        style = "bold cyan";
         disabled = false;
       };
       directory = {
