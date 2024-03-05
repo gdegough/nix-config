9e106ab9b4eb76cff43812ecd13b196cd07f0c05
gdegough <gdegough@gmail.com>
2024-03-05 07:41:33 -0500

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   nixos/gmdegoug.nix

--- nixos/gmdegoug.nix
+++ nixos/gmdegoug.nix
@@ -15,7 +15,7 @@ in
     isNormalUser = true;
     uid = 1000;
     description = "Gregory M. DeGough";
-    shell = pkgs.zsh;
+    shell = pkgs.nushell;
     hashedPasswordFile = "/persist/passwords/gmdegoug";
     extraGroups = [ 
       "systemd-journal" 
