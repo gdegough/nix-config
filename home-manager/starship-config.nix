f6e46990d53968df2f0f48a7e793b00b9a50ada9
gdegough <gdegough@gmail.com>
2024-03-04 20:30:32 -0500

    On branch main  Your branch is ahead of 'origin/main' by 16 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   home-manager/starship-config.nix

--- home-manager/starship-config.nix
+++ home-manager/starship-config.nix
@@ -11,24 +11,29 @@
     enable = true;
     settings = {
       add_newline = false;
-      format = "$directory$line_break\(($os)\)$username@$hostname$character ";
+      format = "[┌](green)($os)($username)[@](bold yellow)($hostname)($directory)$line_break[└─](green)$character";
       character = { 
-        success_symbol = "[➜](bold green)";
-        error_symbol = "[➜](bold red)";
+        success_symbol = "[>](bold green)";
+        error_symbol = "[>](bold red)";
       };
       username = {
-        format = "[$user]($style)";
+        format = "[─\(](green)[$user]($style)";
         show_always = true;
       };
       hostname = {
-        format = "[$ssh_symbol$hostname]($style)";
+        format = "[$ssh_symbol$hostname]($style)[\)─](green)";
         style = "bold yellow";
         trim_at = "";
         ssh_only = false;
       };
       os = {
-        style = "cyan";
+        format = "[─\(](green)[$symbol]($style)[\)─](green)";
+        style = "bold cyan";
         disabled = false;
+      };
+      directory = {
+        format = "[─\(](green)[$path]($style)[$read_only]($read_only_style)[\)](green)";
+        truncation_symbol = "…/";
       };
     };
   };
