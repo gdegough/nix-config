8257cbedfe3c593e11bb699fe486d89192d8a075
gdegough <gdegough@gmail.com>
2024-03-07 10:31:37 -0500

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   nixos/lemurpro/hardware-configuration.nix
    	modified:   nixos/xps13/hardware-configuration.nix

--- nixos/xps13/hardware-configuration.nix
+++ nixos/xps13/hardware-configuration.nix
@@ -85,6 +85,7 @@
 
   nixpkgs.hostPlatform = "x86_64-linux";
   powerManagement.cpuFreqGovernor = "powersave";
+  hardware.video.hidpi.enable - true; # to detect HiDPI screens
   hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
 
   # this folder is where the files will be stored (don't put it in tmpfs)
