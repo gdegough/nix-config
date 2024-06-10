108e7607bf161cdb4c76daafe13a540564479b37
Gregory M. DeGough <gdegough@gmail.com>
2024-06-10 06:54:42 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   nixos/leanangle/configuration.nix

--- nixos/leanangle/configuration.nix
+++ nixos/leanangle/configuration.nix
@@ -190,9 +190,9 @@
     enable = true;
     settings = {
       # Forbid root login through SSH.
-      PermitRootLogin = "no";
+      PermitRootLogin = "yes";
       # Use keys only. Remove if you want to SSH using password (not recommended)
-      PasswordAuthentication = false;
+      PasswordAuthentication = true;
     };
   };
 
