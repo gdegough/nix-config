4a32e9577cba96f067865153847c104574f57b6b
gdegough <gdegough@gmail.com>
2024-06-11 22:20:26 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   nixos/apex/networking.nix

--- nixos/apex/networking.nix
+++ nixos/apex/networking.nix
@@ -13,8 +13,8 @@
     # (the default) this is the recommended approach. When using systemd-networkd it's
     # still possible to use this option, but it's recommended to use it in conjunction
     # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-    useDHCP = lib.mkForce true;
-    interfaces.enp86s0.useDHCP = true;
+    # useDHCP = lib.mkForce true;
+    # interfaces.enp86s0.useDHCP = true;
     # interfaces.wlo1.useDHCP = true;
     hostName = "apex";
     domain = "natcky.rr.com";
@@ -29,7 +29,7 @@
     #   networks."GregsWLAN".psk = "ac4942274cae3be82fa8a05e54fa3a710b85204671631d85304051a8ce99ff00";
     #   extraConfig = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel";
     # };
-    useNetworkd = true;
+    # useNetworkd = true;
     networkmanager.enable = false;
   };
   systemd.network = {
