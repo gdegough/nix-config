92d8bf071111a7ff149c8b132397dd6de03a0feb
gdegough <gdegough@gmail.com>
2024-06-14 05:36:25 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/bash-config.nix
    	modified:   home-manager/nushell-config.nix
    	modified:   home-manager/zsh-config.nix

--- home-manager/zsh-config.nix
+++ home-manager/zsh-config.nix
@@ -227,7 +227,8 @@
       create-input-file = "sed ':a;N;$!ba;s/\\n/ /g'";
       sudo = "sudo ";
       vim = "vim ";
-      power-profile = "sudo system76-power charge-thresholds --profile balanced && sudo system76-power profile battery && brightnessctl set 50%";
+      power-balanced = "sudo system76-power charge-thresholds --profile balanced && sudo system76-power profile balanced && brightnessctl set 50%";
+      power-battery = "sudo system76-power charge-thresholds --profile balanced && sudo system76-power profile battery && brightnessctl set 50%";
       history = "history 0";
     };
   };
