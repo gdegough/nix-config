b65aac90a4bca327ba80a1cb7b9fd51a33443821
gdegough <gdegough@gmail.com>
2024-06-17 04:16:56 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   nixos/pipewire.nix
            commented out sound.enable since it's not relevant for
            pipewire

--- nixos/pipewire.nix
+++ nixos/pipewire.nix
@@ -10,7 +10,7 @@
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  sound.enable = true;
+  # sound.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
