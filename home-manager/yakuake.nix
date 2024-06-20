70b3eb53f3000620e87375e305ce2586e30c9276
gdegough <gdegough@gmail.com>
2024-06-20 07:56:46 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/yakuake.nix
            added autostart config file to only open in kde

--- home-manager/yakuake.nix
+++ home-manager/yakuake.nix
@@ -7,4 +7,22 @@
   home.packages = [
     pkgs.kdePackages.yakuake
   ];
+  home.file = {
+    ".config/autostart/org.kde.yakuake.desktop".text = ''
+      [Desktop Entry]
+      Categories=Qt;KDE;System;TerminalEmulator;
+      Comment=A drop-down terminal emulator based on KDE Konsole technology.
+      DBusActivatable=true
+      Exec=yakuake
+      GenericName=Drop-down Terminal
+      Icon=yakuake
+      Name=Yakuake
+      Terminal=false
+      Type=Application
+      X-DBUS-ServiceName=org.kde.yakuake
+      X-DBUS-StartupType=Unique
+      X-KDE-StartupNotify=false
+      NotShowIn=GNOME;hyprland;sway;
+    '';
+  };
 }
