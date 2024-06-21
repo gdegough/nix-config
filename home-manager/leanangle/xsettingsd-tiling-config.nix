99afb5a49a1e818e9e6fe667b70c6e100fa923df
gdegough <gdegough@gmail.com>
2024-06-21 10:41:16 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/leanangle/xsettingsd-tiling-config.nix

--- home-manager/leanangle/xsettingsd-tiling-config.nix
+++ home-manager/leanangle/xsettingsd-tiling-config.nix
@@ -6,7 +6,7 @@
 {
   home.file = {
     ".config/xsettingsd/xsettingsd-tiling.conf".text = ''
-        Gdk/UnscaledDPI 98304
+        Gdk/UnscaledDPI 147456
         Gdk/WindowScalingFactor 1
         Gtk/ButtonImages 0
         Gtk/CursorThemeName "Adwaita"
@@ -19,7 +19,7 @@
         Net/SoundThemeName ""
         Net/ThemeName "Adwaita"
         Xft/Antialias 1
-        Xft/DPI 98304
+        Xft/DPI 147456
         Xft/Hinting 1
         Xft/HintStyle "hintslight"
         Xft/RGBA "none"
