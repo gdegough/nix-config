1c7b4a51822f53ad15198900300de8c1305cc7e1
gdegough <gdegough@gmail.com>
2024-07-02 20:41:23 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/kde-office.nix

--- home-manager/kde-office.nix
+++ home-manager/kde-office.nix
@@ -8,6 +8,7 @@
     pkgs.kdePackages.kaccounts-providers
     pkgs.kdePackages.kcalc
     pkgs.kdePackages.kdepim-runtime
+    pkgs.kdePackages.kmail-account-wizard
     pkgs.kdePackages.knotes
     pkgs.kdePackages.kongress
     pkgs.kdePackages.kontact
