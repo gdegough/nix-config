86673ecca549eac543c99a07f4164af101acd885
gdegough <gdegough@gmail.com>
2024-07-05 08:16:41 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/kde-admin.nix

--- home-manager/kde-admin.nix
+++ home-manager/kde-admin.nix
@@ -4,6 +4,35 @@
   ...
 }:
 {
+  home.file = {
+    ".config/autostart/org.kde.kgpg.desktop".text = ''
+      # KDE Config File
+      [Desktop Entry]
+      Type=Application
+      Exec=kgpg %U
+      Icon=kgpg
+      X-DocPath=kgpg/index.html
+      MimeType=application/pgp-encrypted;application/pgp-signature;application/pgp-keys;
+      GenericName=Encryption Tool
+      GenericName[en_GB]=Encryption Tool
+      Comment=A GnuPG frontend
+      Comment[en_GB]=A GnuPG frontend
+      Name=KGpg
+      Name[en_GB]=KGpg
+      X-KDE-autostart-condition=kgpgrc:User Interface:AutoStart:false
+      Categories=Qt;KDE;Utility;X-KDE-Utilities-PIM;
+      NotShowIn=sway;hyprland;GNOME;
+    '';
+    ".config/autostart/org.kde.kclockd-autostart.desktop".text = ''
+      [Desktop Entry]
+      DBusActivatable=true
+      Exec=kclockd
+      Name=org.kde.kclockd-autostart
+      Type=Application
+      X-Flatpak=org.kde.kclockd-autostart
+      NotShowIn=sway;hyprland;GNOME;
+    '';
+  };
   home.packages = [
     pkgs.kdePackages.isoimagewriter
     pkgs.kdePackages.kclock
