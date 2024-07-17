736d7edd8043ea85f6fef11ff4c93cadad976b27
gdegough <gdegough@gmail.com>
2024-07-17 07:20:43 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/vim-config.nix

--- home-manager/vim-config.nix
+++ home-manager/vim-config.nix
@@ -7,9 +7,9 @@
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
-  home.packages = [
-    pkgs.powerline
-  ];
+  # home.packages = [
+  #   pkgs.powerline
+  # ];
   home.sessionVariables = {
     VISUAL = "vim";
     EDITOR = "vim";
