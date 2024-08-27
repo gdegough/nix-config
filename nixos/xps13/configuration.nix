42b91f56347ef7000fdb405ff9e1140e4321c804
gdegough <gdegough@gmail.com>
2024-08-27 14:02:42 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   nixos/xps13/configuration.nix

--- nixos/xps13/configuration.nix
+++ nixos/xps13/configuration.nix
@@ -38,8 +38,8 @@
     ## window managers and DEs
     ../tiling-wm-support.nix ## common tiling WM support
     # ../hyprland.nix ## Hyprland WM
-    #../sway.nix ## Sway WM
-    #../kde.nix ## KDE desktop environment
+    ../sway.nix ## Sway WM
+    ../kde.nix ## KDE desktop environment
     ../gnome.nix ## GNOME desktop environment
 
     ## users
