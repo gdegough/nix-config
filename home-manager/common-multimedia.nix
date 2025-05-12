f89172244e5ffc60b8a71fcb32bd0e2c1443c772
gdegough <gdegough@gmail.com>
2025-05-12 10:45:43 -0400

    On branch main Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/common-multimedia.nix

--- home-manager/common-multimedia.nix
+++ home-manager/common-multimedia.nix
@@ -22,4 +22,11 @@
     pkgs.vlc
     # pkgs.vmpk
   ];
+  # For spotify
+  networking.firewall.allowedTCPPorts = [
+    57621 # to sync local tracks from your filesystem with mobile devices in the same network
+  ];
+  networking.firewall.allowedUDPPorts = [
+    5353 # to enable discovery of cast and possibly other connect devices in the same network
+  ];
 }
