184e5c0a072daf14b3131f962167e9e81f5c6287
gdegough <gdegough@gmail.com>
2025-05-12 01:23:27 -0400

    On branch main Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   home-manager/starship.nix

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -7,8 +7,6 @@
   home.packages = [
     pkgs.starship
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-    pkgs.fira-code
-    pkgs.droid-sans-mono
   ];
   programs.starship = {
     enable = true;
